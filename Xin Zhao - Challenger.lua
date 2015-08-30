d79b9c7679972e29efa2a46c0c076f9e7389b8a4
Titos <jakeoceanus@gmail.com>
2015-08-30 00:38:44 -0600

    Added Menu

--- Xin Zhao - Challenger.lua
+++ Xin Zhao - Challenger.lua
@@ -45,3 +45,51 @@ if AUTOUPDATE then
 end
 
 if myHero.charName ~= "XinZhao" then return end
+
+function OnLoad()
+	print("<b><font color=\"#00FFFF\">Titos and GeorgeDude: <font color=\"#B40404\">Xin Zhao <font color=\"#FFFF00\">- <font color=\"#B40404\">Challenger <font color=\"#FFFF00\">["..version.."] <font color=\"#B40404\">Loaded.</font>")
+	Menu()
+	DelayAction(function() LoadOrbwalker() end, 10)
+end
+
+function LoadOrbwalker()
+	if _G.AutoCarry ~= nil then
+		SACLoaded = true
+		Settings.Orbwalker:addParam("info", "Detected SAC", SCRIPT_PARAM_INFO, "")
+		_G.AutoCarry.Skills:DisableAll()
+		PrintChat("<font color=\"#800080\"><b>SAC: </b></font> <font color=\"#FFFF00\">Loaded</font>")
+	else
+		if not FileExist(LIB_PATH.."SxOrbWalk.lua") then
+			LuaSocket = require("socket")
+			ScriptSocket = LuaSocket.connect("sx-bol.eu", 80)
+			ScriptSocket:send("GET /BoL/TCPUpdater/GetScript.php?script=raw.githubusercontent.com/Superx321/BoL/master/common/SxOrbWalk.lua&rand="..tostring(math.random(1000)).." HTTP/1.0\r\n\r\n")
+			ScriptReceive, ScriptStatus = ScriptSocket:receive('*a')
+			ScriptRaw = string.sub(ScriptReceive, string.find(ScriptReceive, "<bols".."cript>")+11, string.find(ScriptReceive, "</bols".."cript>")-1)
+			ScriptFileOpen = io.open(LIB_PATH.."SxOrbWalk.lua", "w+")
+			ScriptFileOpen:write(ScriptRaw)
+			ScriptFileOpen:close()
+		end
+		require("SxOrbwalk")
+		SxOrbLoaded = true
+		_G.SxOrb:LoadToMenu(Settings.Orbwalker)
+	end
+end
+
+function GetOrbTarget()
+	TargetSelector:update()
+	if SACLoaded then return _G.AutoCarry.Crosshair:GetTarget() end
+	return TargetSelector.target
+end
+
+function Menu()
+	Settings = scriptConfig("Xin Zhao - Challenger "..version.."", "XinZhao")
+	
+	Settings:addSubMenu("["..myHero.charName.."] - Keybind Settings", "Keybind")
+		Settings.Keybind:addParam("ComboKey", "Combo Key:", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+end
+
+
+
+
+
+
